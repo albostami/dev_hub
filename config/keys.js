59e5dbae4dbb86e74b425f941246122390606ef2
Ehab Bustami <albostami@gmail.com>
2018-08-18 21:23:40 +0400

    Initial express server with router setup

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI : 'mongodb://ehab:ehab123@ds161179.mlab.com:61179/dev_hub'
+};
+
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")          ;
+const mongoose = require("mongoose")        ;
+
+const profile = require('./routes/api/profile')     ;
+const users_auth = require("./routes/api/users")         ;
+const posts = require("./routes/api/posts")         ;
+
+const app = express();
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("<h1>Hello world!</h1>"));
+
+//Use Routes
+app.use('/api/users', users_auth)        ;
+app.use('/api/profile', profile)    ;
+app.use('/api/posts', posts)        ;
+ 
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
