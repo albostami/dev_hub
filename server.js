8a01918536483c753118551d4802f31f4b93b6c0
Ehab Bustami <albostami@gmail.com>
2018-08-22 00:14:13 +0400

    Registration router

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require("express")          ;
-const mongoose = require("mongoose")        ;
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const profile = require('./routes/api/profile')     ;
-const users_auth = require("./routes/api/users")         ;
-const posts = require("./routes/api/posts")         ;
+const profile = require("./routes/api/profile");
+const users_auth = require("./routes/api/users");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser middle
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
@@ -19,11 +24,10 @@ mongoose
 app.get("/", (req, res) => res.send("<h1>Hello world!</h1>"));
 
 //Use Routes
-app.use('/api/users', users_auth)        ;
-app.use('/api/profile', profile)    ;
-app.use('/api/posts', posts)        ;
- 
+app.use("/api/users", users_auth)     ;
+app.use("/api/profile", profile)      ;
+app.use("/api/posts", posts)          ;
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000 ;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
